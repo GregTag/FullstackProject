0d75f6d587cafa66e88ce8ea695fff4e243eddc6
Alexander Khotskov <khotskov.aa@phystech.edu>
2023-10-20 01:44:29 +0300

    Finish home and auth pages

--- frontend/src/common/Header.jsx
+++ frontend/src/common/Header.jsx
@@ -1,19 +1,25 @@
 import { Form, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from '../logic/user';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoggedIn, logout } from '../logic/user';
 import './Header.css';
 
 function Header() {
     const isLoggedIn = useSelector(selectIsLoggedIn);
+    const dispatch = useDispatch();
     return (
         <header>
             <Link to="/"><h2 className='title'>Universal Media Organizer</h2></Link>
-            <Form className='search-bar'>
-                <input type="text" placeholder="Search..." />
+            <Form action='/search' method='GET' className='search-bar'>
+                <input type="text" name='query' placeholder="Search..." />
                 <button type="submit" className='button'>Search</button>
             </Form>
             {isLoggedIn ? (
-                <Link to="/profile" className='button'>My Profile</Link>
+                <div className='adaptive-flex'>
+                    <Link to="/profile" className='button'>My Profile</Link>
+                    <button onClick={() => dispatch(logout())} className='button'>
+                        <span class="material-symbols-outlined">logout</span>
+                    </button>
+                </div>
             ) : (
                 <Link to="/auth" className='button'>Login</Link>
             )}
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,9 +1,66 @@
 import './Auth.css'
+import { login } from '../logic/user'
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+function Login({ onRegister }) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    return (
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            // const { username, password } = event.target.elements;
+            dispatch(login());
+            navigate('/profile');
+        }}>
+            <label htmlFor='username'>Username:</label>
+            <input type='text' name='username' />
+            <label htmlFor='password'>Password:</label>
+            <input type='password' name='password' />
+            <div className='row-container'>
+                <button type='submit' className='button'>Login</button>
+                <button onClick={onRegister} className='button'>Register</button>
+            </div>
+        </form >
+    )
+}
+
+function Register() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    return (
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            const { password, repeated } = event.target.elements;
+            if (password.value !== repeated.value) {
+                alert('Passwords do not match!');
+                return;
+            }
+
+            dispatch(login());
+            navigate('/profile');
+        }}>
+            <label htmlFor='username'>Username:</label>
+            <input type='text' name='username' />
+            <label htmlFor='password'>Password:</label>
+            <input type='password' name='password' />
+            <label htmlFor='repeated'>Repeat Password:</label>
+            <input type='password' name='repeated' />
+            <div className='row-container'>
+                <button type='submit' className='button'>Register</button>
+            </div>
+        </form>
+    )
+}
+
 
 function AuthPage() {
+    const [registering, setRegistering] = useState(false);
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className='central-panel'>
+            <Link to="/"><h1>Universal Media Organizer</h1></Link>
+            {registering ? <Register /> : <Login onRegister={() => setRegistering(true)} />}
         </div>
     )
 }
